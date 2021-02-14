f058e2acce979fdb5efdd83a5712efd7d175484f
Abhisht Jindal <64840557+Abhisht23@users.noreply.github.com>
2021-02-14 12:25:21 +0530

    Add files via upload

---
+++ Functions/25.cpp
@@ -0,0 +1,14 @@
+//using function
+#include <iostream>
+using namespace std;
+void display()
+{
+cout<<"Hello";
+}
+int main()
+{
+display();
+return 0;
+}
+
+
---
+++ Functions/31.cpp
@@ -0,0 +1,16 @@
+//call by value
+#include<iostream>
+using namespace std;
+void swap(int a, int b)
+	{
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+	}
+		int main()
+	{
+		int x=10, y=20;
+		swap(x,y);
+		cout<<x<<endl<<y;
+	}
