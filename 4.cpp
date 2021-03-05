6db0f296cd5d47cd23f8a8ec32ba718fe398f491
Abhisht Jindal <64840557+Abhisht23@users.noreply.github.com>
2021-03-05 23:17:56 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using  namespace std;
+int main()
+{
+	int x,y;
+	cout<<"Enter the value of x and y"<<endl;
+	cin>>x>>y;
+	if(x>y)
+	cout<<x<<"x is max"<<endl;
+	else
+	cout<<y<<"y is max"<<endl;
+	return 0;
+}
